80aa8d89d7973fa14818a867b37603a886ce4069
joshaccount <joshwiselim@gmail.com>
2026-02-04 11:05:27 +0700

    week01: initial setup and procedural code

---
+++ src/oop_105516_JoshReaganWiselim/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_105516_JoshReaganWiselim.week01
+
+fun main() {
+    var name: String = "Josh Reagan Wiselim"
+    var score: Int = 100
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
