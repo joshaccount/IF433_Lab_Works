84d5a2c34e4f76ad064a45861f0931ba501b7ccf
joshaccount <joshwiselim@gmail.com>
2026-02-04 11:54:01 +0700

    week01: (tugas 2) setup GameStore file

---
+++ src/oop_105516_JoshReaganWiselim/week01/GameStore.kt
@@ -0,0 +1,8 @@
+package oop_105516_JoshReaganWiselim.week01
+
+fun main() {
+    val gameTitle: String = "FC26"
+    val price: Int = 800000
+
+    println("Game Title = $gameTitle, Price = $price")
+}
