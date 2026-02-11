2bca249d1fc7f06f0697a50d87bde15a83032004
joshaccount <joshwiselim@gmail.com>
2026-02-11 11:50:12 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_105516_JoshReaganWiselim/week02/Main.kt
@@ -0,0 +1,28 @@
+package oop_105516_JoshReaganWiselim.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine() // Bersihkan buffer newline (Penyakit klasik Scanner!)
+
+    // Validasi di sisi pemanggil (Main)
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
