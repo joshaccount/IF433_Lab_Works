cc94bcab83454408913bd5480a1db3378cd7c5fa
joshaccount <joshwiselim@gmail.com>
2026-02-18 11:31:29 +0700

    week03: cleanup code

--- src/oop_105516_JoshReaganWiselim/week03/Main.kt
+++ src/oop_105516_JoshReaganWiselim/week03/Main.kt
@@ -3,15 +3,11 @@ package oop_105516_JoshReaganWiselim.week03
 fun main() {
     val e = Employee("Budi")
 
-    // 1. Test Validasi Salary
-    e.salary = -1000 // Harusnya print Error
+    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    // 2. Test Encapsulation
     e.increasePerformance()
-    // e.performanceRating = 5 // Coba uncomment, pasti Merah (error)
 
-    // 3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
