b577c7baeda6d7eae3baa0f7eeb2e0b190a10d9d
joshaccount <joshwiselim@gmail.com>
2026-02-18 10:17:23 +0700

    week03: simulate stack overflow error in setter

--- src/oop_105516_JoshReaganWiselim/week03/Employee.kt
+++ src/oop_105516_JoshReaganWiselim/week03/Employee.kt
@@ -2,4 +2,10 @@ package oop_105516_JoshReaganWiselim.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            // PERINGATAN: KODE DI BAWAH INI SALAH & BERBAHAYA!
+            // Kita memanggil setter di dalam setter (Recursive)
+            this.salary = value
+        }
 }
---
+++ src/oop_105516_JoshReaganWiselim/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_105516_JoshReaganWiselim.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000 // Ini akan meledakkan program Anda
+}
