cc0fa6b6bf41c858ab3f6c07201453992450a031
joshaccount <joshwiselim@gmail.com>
2026-03-04 10:58:29 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_105516_JoshReaganWiselim/week05/EWallet.kt
@@ -0,0 +1,17 @@
+package oop_105516_JoshReaganWiselim.week05
+
+class EWallet (accountName: String, var balance: Double) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("EWallet $accountName: Payment sukses men! Sisa saldo $balance")
+        } else {
+            println("EWallet $accountName: Saldo tidak cukup men! Gabisa payment")
+        }
+    }
+
+    fun topup (amount: Double) {
+        balance += amount
+        println("EWallet $accountName: Berhasil toup menn! Saldo sekarang $balance")
+    }
+}
