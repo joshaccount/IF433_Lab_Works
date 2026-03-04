50c322025f013842730c892aa5f7950437a22a39
joshaccount <joshwiselim@gmail.com>
2026-03-04 10:21:27 +0700

    week05: create Dosen class inheriting Pegawai

---
+++ src/oop_105516_JoshReaganWiselim/week05/Dosen.kt
@@ -0,0 +1,13 @@
+package oop_105516_JoshReaganWiselim.week05
+
+class Dosen(nama: String, val nidn: String) : Pegawai(nama) {
+    // WAJIB di-override karena fungsi bekerja() bersifat abstract di Parent
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+
+    // Fungsi unik/spesifik yang hanya dimiliki Dosen
+    fun mengajar() {
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
+}
