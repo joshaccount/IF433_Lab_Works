22b74fb005a33b32c5e4e75ee573cc047cf5ed13
joshaccount <joshwiselim@gmail.com>
2026-03-04 11:18:09 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_105516_JoshReaganWiselim/week05/MainWallet.kt
+++ src/oop_105516_JoshReaganWiselim/week05/MainWallet.kt
@@ -10,5 +10,18 @@ fun main() {
 
         println("\nMencoba transaksi 75000.0")
         payment.processPayment(75000.0)
+
+        when (payment) {
+
+            is EWallet -> {
+                payment.topUp(50000.0)
+                payment.processPayment(75000.0)
+            }
+
+            is CreditCard -> {
+                println("Mengecek sisa limit kartu...")
+                println("Limit terpakai: ${payment.usedAmount} / ${payment.limit}")
+            }
+        }
     }
 }
