02df1e50823124f9c5a9fb7ed51560637ff1e208
joshaccount <joshwiselim@gmail.com>
2026-03-11 10:36:41 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_105516_JoshReaganWiselim/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_105516_JoshReaganWiselim.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan")
+    }
+}
