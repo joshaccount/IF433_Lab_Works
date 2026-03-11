b4426451b772b2775968e240edad8c2f62daf217
joshaccount <joshwiselim@gmail.com>
2026-03-11 10:40:40 +0700

    week06: create PaymentMethod contract

---
+++ src/oop_105516_JoshReaganWiselim/week06/PaymentMethod.kt
@@ -0,0 +1,5 @@
+package oop_105516_JoshReaganWiselim.week06
+
+interface PaymentMethod {
+    fun pay(amount: Double)
+}
--- src/oop_105516_JoshReaganWiselim/week06/Smartphone.kt
+++ src/oop_105516_JoshReaganWiselim/week06/Smartphone.kt
@@ -1,6 +1,5 @@
 package oop_105516_JoshReaganWiselim.week06
 
-// ERROR: CLass Smartphone inherits multiple implementations of turnOn()
 class Smartphone : Camera, Phone {
     // Manually override to resolve ambiguity
     override fun turnOn() {
