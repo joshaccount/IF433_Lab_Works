e9a5c50da397f3997cfc4c8decd2db97a567e8c5
joshaccount <joshwiselim@gmail.com>
2026-03-11 11:15:30 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_105516_JoshReaganWiselim/week06/MainTask.kt
@@ -0,0 +1,8 @@
+package oop_105516_JoshReaganWiselim.week06
+
+fun main() {
+    println("\n=== SMART HOME SYSTEM ===")
+    val lamp = SmartLamp("SL-001", "Philips Hue Living Room")
+    val speaker = SmartSpeaker("SS-001", "Google Nest Dapur")
+    val cctv = SmartCCTV("SC-001", "Garage Watch Cam")
+}
