7c29ab3a0ae97d24ebe0273124ed4d0636879ca9
joshaccount <joshwiselim@gmail.com>
2026-03-25 12:43:52 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_105516_JoshReaganWiselim/week07/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_105516_JoshReaganWiselim.week07
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
