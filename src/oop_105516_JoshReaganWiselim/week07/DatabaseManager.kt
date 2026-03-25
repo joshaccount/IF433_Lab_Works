59cdd6085f17c7aa2832bc5859ff42f9d2d974f6
joshaccount <joshwiselim@gmail.com>
2026-03-25 12:00:52 +0700

    week07: create singleton DatabaseManager

---
+++ src/oop_105516_JoshReaganWiselim/week07/DatabaseManager.kt
@@ -0,0 +1,10 @@
+package oop_105516_JoshReaganWiselim.week07
+
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect() {
+        connectionStatus = "Connected to Server"
+        println("Database is ready.")
+    }
+}
