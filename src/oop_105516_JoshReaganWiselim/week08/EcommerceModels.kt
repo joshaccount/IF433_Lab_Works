710d83d6393377ace43e191c514b7a30d6ee48e2
joshaccount <joshwiselim@gmail.com>
2026-04-15 11:17:37 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_105516_JoshReaganWiselim/week08/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_105516_JoshReaganWiselim.week08
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
