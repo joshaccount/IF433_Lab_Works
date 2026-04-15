87eced6912818e1120002b713567e4cc7354a275
joshaccount <joshwiselim@gmail.com>
2026-04-15 10:34:19 +0700

    week08: implement safe call with let block

--- src/oop_105516_JoshReaganWiselim/week08/OrderModels.kt
+++ src/oop_105516_JoshReaganWiselim/week08/OrderModels.kt
@@ -3,4 +3,4 @@ package oop_105516_JoshReaganWiselim.week08
 class City(val name: String)
 class Address(val city: City?)
 class DeliveryDetails(val address: Address?)
-class order(val deliveryDetails: DeliveryDetails?, val totalPRice: Int?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
