c7dbfc8d0edbc85440d2f9e15fb9c3d9371e52ac
joshaccount <joshwiselim@gmail.com>
2026-04-22 10:46:38 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_105516_JoshReaganWiselim/week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_105516_JoshReaganWiselim.week09
+
+data class TradeLog (
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
