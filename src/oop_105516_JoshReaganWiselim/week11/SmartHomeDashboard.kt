26ccb423aca889e2fec91c1cbb5e4067e5d8ed7d
joshaccount <joshwiselim@gmail.com>
2026-05-06 10:57:23 +0700

    week11: (task) configure AC unit using run

--- src/oop_105516_JoshReaganWiselim/week11/SmartHomeDashboard.kt
+++ src/oop_105516_JoshReaganWiselim/week11/SmartHomeDashboard.kt
@@ -20,4 +20,16 @@ fun main(){
         println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
+
+    println("\n[SETUP] Mengkonfigurasi sistem HVAC...")
+    val acUnit = run {
+        SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
+    }
+    homeDevices.add(acUnit)
+    println("(LOG) AC unit ditambahkan: ${acUnit.name}")
+
+    homeDevices.add(
+        SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10)
+    )
+    println("(LOG) Alat pakan peliharaan ditambahkan: Picolo's Auto Feeder")
 }
