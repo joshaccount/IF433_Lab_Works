d0caca8f212cbc703e4cccaf4a12ee695b877ccb
joshaccount <joshwiselim@gmail.com>
2026-05-13 10:10:02 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_105516_JoshReaganWiselim/week12/SmartFeeder.kt
+++ src/oop_105516_JoshReaganWiselim/week12/SmartFeeder.kt
@@ -17,4 +17,28 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    println("=== JADWAL MAKAN PAGI ===")
+
+    println("Stok saat ini: $currentKibbleStock gr | Porsi diminta: 80 gr")
+
+    try {
+        val newStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+        currentKibbleStock = newStock
+        println("Makan pagi sukses! Sisa stok kibble: $currentKibbleStock gr")
+
+    } catch (e: DispenserJamException) {
+        println("ERROR HARDWARE: ${e.message}")
+        println("Harap periksa dan bersihkan dispenser secara manual.")
+    } catch (e: FoodEmptyException) {
+        println("ERROR STOK: ${e.message}")
+        println("Harap isi ulang wadah kibble segera!")
+    } catch (e: Exception) {
+        println("ERROR UMUM: ${e.message}")
+        println("Terjadi kesalahan tidak terduga pada sistem.")
+    }
 }
