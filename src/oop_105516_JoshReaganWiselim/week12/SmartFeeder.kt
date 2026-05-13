e5e3336a40b44b2ea471c19f8598e9cbb6f0d7ed
joshaccount <joshwiselim@gmail.com>
2026-05-13 10:03:00 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_105516_JoshReaganWiselim/week12/SmartFeeder.kt
@@ -0,0 +1,5 @@
+package oop_105516_JoshReaganWiselim.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+}
