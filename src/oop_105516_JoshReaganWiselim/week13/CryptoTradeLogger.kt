9e645e820ade38b5821daf7024ed0a8649c05463
joshaccount <joshwiselim@gmail.com>
2026-05-20 09:57:15 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_105516_JoshReaganWiselim/week13/CryptoTradeLogger.kt
@@ -0,0 +1,11 @@
+package oop_105516_JoshReaganWiselim.week13
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
